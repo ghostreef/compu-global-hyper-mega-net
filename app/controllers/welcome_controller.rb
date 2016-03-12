1ff881777cd50649a514c85210c26ec59087669a
John Setiawan <jsetiawan@capterra.com>
2016-03-11 19:55:00 -0500

    Generate fake table data.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,19 @@
 class WelcomeController < ApplicationController
   def index
 
+    @headers = ['EIN', 'Title', 'Name', 'Email', 'Company Name', 'Phone Number', 'City']
+
+    @rows = 60.times.map do
+      [
+          Faker::Company.ein,
+          Faker::Name.title,
+          Faker::Name.name,
+          Faker::Internet.email,
+          Faker::Name.name,
+          Faker::Number.number(10),
+          Faker::Address.city
+      ]
+    end
+
   end
 end
